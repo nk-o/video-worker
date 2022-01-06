0ef9f074c7e748ff0d1b803168945bcf7a67811d
Nikita <nkdevinfo@gmail.com>
2022-01-06 21:17:51 +0300

    added prettier updated deps

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,18 +1,19 @@
-const gulp = require( 'gulp' );
-const $ = require( 'gulp-load-plugins' )();
-const del = require( 'del' );
-const named = require( 'vinyl-named' );
-const webpack = require( 'webpack-stream' );
-const { data } = require( 'json-file' ).read( './package.json' );
+/* eslint-disable function-paren-newline */
+const gulp = require('gulp');
+const $ = require('gulp-load-plugins')();
+const del = require('del');
+const named = require('vinyl-named');
+const webpack = require('webpack-stream');
+const { data } = require('json-file').read('./package.json');
 
-const webpackconfig = require( './webpack.config.js' );
+const webpackconfig = require('./webpack.config');
 
 function getHeader() {
-    return `/*!
+  return `/*!
  * Name    : Video Worker
- * Version : ${ data.version }
- * Author  : ${ data.author }
- * GitHub  : ${ data.homepage }
+ * Version : ${data.version}
+ * Author  : ${data.author}
+ * GitHub  : ${data.homepage}
  */
 `;
 }
@@ -20,42 +21,47 @@ function getHeader() {
 /**
  * Error Handler for gulp-plumber
  */
-function errorHandler( err ) {
-    // eslint-disable-next-line no-console
-    console.error( err );
-    this.emit( 'end' );
+function errorHandler(err) {
+  // eslint-disable-next-line no-console
+  console.error(err);
+  this.emit('end');
 }
 
 /**
  * Clean Task
  */
-gulp.task( 'clean', () => del( [ 'dist' ] ) );
+gulp.task('clean', () => del(['dist']));
 
 /**
  * JS Task
  */
-gulp.task( 'js', () => (
-    gulp.src( [ 'src/*.js', '!src/*.esm.js' ] )
-        .pipe( $.plumber( { errorHandler } ) )
-        .pipe( named() )
-        .pipe( webpack( {
-            config: webpackconfig,
-        } ) )
-        .pipe( $.header( getHeader() ) )
-        .pipe( gulp.dest( 'dist' ) )
-        .pipe( $.rename( { suffix: '.min' } ) )
-        .pipe( $.uglify( {
-            output: {
-                comments: /^!/,
-            },
-        } ) )
-        .pipe( $.sourcemaps.write( '.' ) )
-        .pipe( gulp.dest( 'dist' ) )
-) );
+gulp.task('js', () =>
+  gulp
+    .src(['src/*.js', '!src/*.esm.js'])
+    .pipe($.plumber({ errorHandler }))
+    .pipe(named())
+    .pipe(
+      webpack({
+        config: webpackconfig,
+      })
+    )
+    .pipe($.header(getHeader()))
+    .pipe(gulp.dest('dist'))
+    .pipe($.rename({ suffix: '.min' }))
+    .pipe(
+      $.uglify({
+        output: {
+          comments: /^!/,
+        },
+      })
+    )
+    .pipe($.sourcemaps.write('.'))
+    .pipe(gulp.dest('dist'))
+);
 
 /**
  * Build (default) Task
  */
-gulp.task( 'build', gulp.series( 'clean', 'js' ) );
+gulp.task('build', gulp.series('clean', 'js'));
 
-gulp.task( 'default', gulp.series( 'build' ) );
+gulp.task('default', gulp.series('build'));
