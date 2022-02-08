86a95df29a4561e70fb6623b93d01c33e4ee3879
Nikita <nkdevinfo@gmail.com>
2022-02-09 00:17:36 +0300

    removed global dependency
    added utils for global and deferred

--- src/video-worker.esm.js
+++ src/video-worker.esm.js
@@ -1,36 +1,5 @@
-import global from 'global';
-
-// Deferred
-// thanks http://stackoverflow.com/questions/18096715/implement-deferred-object-without-using-jquery
-function Deferred() {
-  this.doneCallbacks = [];
-  this.failCallbacks = [];
-}
-
-Deferred.prototype = {
-  execute(list, args) {
-    let i = list.length;
-    // eslint-disable-next-line no-param-reassign
-    args = Array.prototype.slice.call(args);
-
-    while (i) {
-      i -= 1;
-      list[i].apply(null, args);
-    }
-  },
-  resolve(...args) {
-    this.execute(this.doneCallbacks, args);
-  },
-  reject(...args) {
-    this.execute(this.failCallbacks, args);
-  },
-  done(callback) {
-    this.doneCallbacks.push(callback);
-  },
-  fail(callback) {
-    this.failCallbacks.push(callback);
-  },
-};
+import global from './utils/global';
+import Deferred from './utils/deferred';
 
 let ID = 0;
 let YoutubeAPIadded = 0;
@@ -40,7 +9,7 @@ let loadingVimeoPlayer = 0;
 const loadingYoutubeDefer = new Deferred();
 const loadingVimeoDefer = new Deferred();
 
-export default class VideoWorker {
+class VideoWorker {
   constructor(url, options) {
     const self = this;
 
@@ -878,3 +847,5 @@ export default class VideoWorker {
     }
   }
 }
+
+export default VideoWorker;
